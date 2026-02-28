829b3c1ab09ac41a3a44c7c4515ddecf1df509b8
nadine.sorinder@gmail.com <nadine.sorinder@gmail.com>
2026-02-28 18:54:00 +0100

    easter egg nr 2 added

--- easteregg.js
+++ easteregg.js
@@ -9,3 +9,26 @@ secretSpot.addEventListener('click', () => {
     document.body.style.color = "white";
 }
 );
+
+/*Easter egg nr2 - writing the secret code */
+
+let input = "";
+const secretCode = "lol";
+
+window.addEventListener("keydown", (e) => {
+    // check if key is of "correct value"(a-z) and add every new key to the input-string
+    if (e.key.length === 1 && /[a-z]/i.test(e.key)) {
+        input += e.key.toLowerCase();
+    }
+
+    /*if input gets longer than secret code, this trims the start by using substring to keep only most resent keys.*/
+    if (input.length > secretCode.length) {
+        input = input.substring(input.length - secretCode.length);
+    }
+
+    if (input === secretCode) {
+        alert("Pretend I wrote something really funny :D");
+        input = ""; /*reset after success*/
+    }
+}
+);
